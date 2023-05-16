f37259602d5d2c2518dbad7aab3e6a137330198e
DonMcQueen <donniemcqueen@gmail.com>
2023-05-15 23:54:02 -0400

    practicing python lists

---
+++ camping.py
@@ -0,0 +1,22 @@
+# We are going on a camping trip. Here is a list of things that we need.
+
+camping_stuff = "tent, sleeping bags, water. raspberry pi, coffee, knife, ethernet cable, flash drive, beard oil, marshmallows"
+
+print(camping_stuff)
+
+# Python List
+
+camping_list = ["tent", "sleeping bags", "water", "raspberry pi", "coffee", "knife", "ethernet cable", "flash drive", "beard oil", "marshmallows"]
+
+print(type(camping_list))
+
+camp_site = ["Crystal Lake", 404, 89.3, True]
+
+me = camping_list[4]
+
+you = camping_list[-2]
+
+print(me)
+print(you)
+
+print(camping_list)
