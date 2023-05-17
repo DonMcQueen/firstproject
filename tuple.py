21e13408f21d487daf6bec2d74327b1e5a099247
DonMcQueen <donniemcqueen@gmail.com>
2023-05-16 23:24:48 -0400

    practice using tuples

---
+++ tuple.py
@@ -0,0 +1,28 @@
+alist = ["bernard", "hackwell", 92]
+
+alist[0] = "Don"
+
+print(alist)
+
+atuple = ("bernard", "hackwell", 92)
+
+print(atuple)
+
+
+
+# List
+
+coffee = ["on-call", "default route", "sudo", "200 ok"]
+
+youtubers = ["john hammond", "Jeremyi's it lab", "david bombal", "NetworkChuck"]
+
+
+# Tuples
+
+youtuber = ("John Hammond", 519000, "hacking")
+
+(name, subs, content) = youtuber
+
+print(name)
+print(subs)
+print(content)
